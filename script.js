396e111289325567fed5c215f03b9e82fc240aad
zainulhassan313 <siddquizain38@gmail.com>
2024-11-25 13:07:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var one = prompt("Enter a positive number:");
+console.log("Positive:")
+if(one>0){
+    console.log("your number: ",one);
+    console.log("round of value: ", Math.round(one));
+    console.log("floor value: ", Math.floor(one));
+    console.log("ceil value: ", math.ceil(one));
+}
+else{
+    console.log("your number is wrong.")
+}
+
+
+
+
+var two = prompt("Enter a negative number:");
+console.log("Negative:")
+if(two<0){
+    console.log("your number: ",two);
+    console.log("round of value: ", Math.round(two));
+    console.log("floor value: ", Math.floor(two));
+    console.log("ceil value: ", math.ceil(two));
+}
+else{
+    console.log("your number is wrong.")
+}
